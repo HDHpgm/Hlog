0ae1b025d5684ae4fb546da23b6f57d060e44813
HDHpgm <gb78@naver.com>
2022-03-04 21:24:13 +0900

    ci-cd 테스트3

--- src/test/java/com/dev/hlog/HlogApplicationTests.java
+++ src/test/java/com/dev/hlog/HlogApplicationTests.java
@@ -1,15 +1,15 @@
-package com.dev.hlog;
-
-
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HlogApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.dev.hlog;
+//
+//
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HlogApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
